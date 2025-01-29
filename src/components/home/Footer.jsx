97f929cdb2c41c63e9222139f6f66634631f5525
XXXGokuX <112057951+XXXGokuX@users.noreply.github.com>
2025-01-29 23:46:48 +0530

    Update Footer.jsx

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -22,21 +22,12 @@ function Footer() {
                                     <path d="M29 0h-26c-1.65 0-3 1.35-3 3v26c0 1.65 1.35 3 3 3h13v-14h-4v-4h4v-2c0-3.306 2.694-6 6-6h4v4h-4c-1.1 0-2 0.9-2 2v2h6l-1 4h-5v14h9c1.65 0 3-1.35 3-3v-26c0-1.65-1.35-3-3-3z"></path>
                                 </svg>
                             </a>
-                            <a href="https://twitter.com/eyebuydirect/" target="_blank" rel="noopener noreferrer" title="Follow us on Twitter">
-                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M908.39 473.037c57.293-4.71 96.154-30.771 111.104-66.099-20.685 12.698-84.838 26.522-120.269 13.363-1.741-8.346-3.686-16.23-5.581-23.398-26.982-99.123-119.45-178.995-216.32-169.37 7.834-3.174 15.77-6.093 23.706-8.755 10.65-3.84 73.216-13.978 63.386-36.096-8.294-19.405-84.736 14.694-99.123 19.149 18.995-7.168 50.432-19.456 53.76-41.267-29.082 3.994-57.651 17.766-79.77 37.734 7.987-8.602 14.029-19.046 15.309-30.31-77.619 49.613-122.982 149.555-159.642 246.528-28.774-27.853-54.323-49.818-77.21-62.054-64.256-34.458-141.107-70.451-261.734-115.2-3.738 39.936 19.712 92.979 87.245 128.307-14.643-1.997-41.37 2.406-62.771 7.526 8.704 45.722 37.171 83.405 114.278 101.632-35.277 2.304-53.453 10.291-69.99 27.597 16.026 31.795 55.194 69.222 125.594 61.542-78.285 33.741-31.898 96.256 31.795 86.938-108.698 112.23-280.013 103.987-378.419 10.138 256.87 350.054 815.258 207.002 898.509-130.15 62.362 0.512 99.021-21.606 121.702-45.978-35.891 6.042-87.91-0.256-115.558-11.776z"></path>
+                            <a href="https://www.google.com/maps/search/EyeBuyDirect/" target="_blank" rel="noopener noreferrer" title="Find us on Google Business">
+                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm.21 17.93c-1.5-.1-3.22-.64-4.31-1.41.52-.86 1.66-1.47 2.93-1.49 1.41-.02 2.8.58 3.69 1.45-.96.59-2.13 1.34-3.55 1.45zm2.92-4.11c-.63-.49-1.68-.91-2.94-.9-1.48.01-2.83.64-3.53 1.42-.9-.91-1.5-2.16-1.5-3.52h13c0 1.37-.6 2.61-1.5 3.52-.54-.6-1.19-1.02-1.53-1.27zm2.87-4.82H6.01c-.02-.21-.02-.42-.02-.63C6 8.28 8.28 6 11.25 6c2.96 0 5.25 2.28 5.25 5.11 0 .21-.01.42-.02.63z"/>
                                 </svg>
                             </a>
-                            <a href="https://www.youtube.com/EyeBuyDirect/" target="_blank" rel="noopener noreferrer" title="Follow us on YouTube">
-                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M896 138.24h-768c-71.68 0-128 56.32-128 128v512c0 66.56 56.32 128 128 128h773.12c66.56 0 128-56.32 128-128v-512c-5.12-71.68-61.44-128-133.12-128zM665.6 547.84l-235.52 163.84c-15.36 10.24-40.96 0-40.96-20.48v-327.68c0-20.48 20.48-30.72 40.96-20.48l235.52 163.84c15.36 10.24 15.36 30.72 0 40.96z"></path>
-                                </svg>
-                            </a>
-                            <a href="https://www.tiktok.com/@eyebuydirect" target="_blank" rel="noopener noreferrer" title="Follow us on TikTok">
-                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M20.625 9.04613C18.7577 9.04613 16.8919 8.48787 15.3752 7.37V14.9641C15.3752 18.7619 12.342 22 8.37375 22C4.40825 22 1.375 18.8733 1.375 14.9641C1.375 11.0564 4.40825 7.92825 8.37375 7.92825C8.72438 7.92825 9.19187 7.92825 9.5425 8.041V12.0615C9.19187 11.9488 8.84125 11.8374 8.37375 11.8374C6.62475 11.8374 5.34188 13.2894 5.34188 14.8527C5.34188 16.6402 6.74163 17.8681 8.37512 17.8681C10.0086 17.8681 11.4084 16.4161 11.4084 14.8527V0H15.2584C15.2584 2.904 17.5917 5.24837 20.3913 5.24837L20.625 9.0475" fill="white"></path>
-                                </svg>
-                            </a>
+
                         </div>
                         <div class="flex md:flex-col gap-4 mt-8">
                             <a target="_blank" rel="nofollow noopener noreferrer" href="https://www.resellerratings.com">
@@ -85,18 +76,18 @@ function Footer() {
                             <li><a href="https://www.eyebuydirect.com/affiliate-program" class="hover:underline">Affiliate Program</a></li>
                             <li><a href="https://www.eyebuydirect.com/student-discount" class="hover:underline">Student Discount</a></li>
                         </ul> */}
-                        <h3 class="text-lg font-bold mt-8 mb-4">Our Brands</h3>
+                        <h3 class="text-lg font-bold mb-4">Our Brands</h3>
                         <ul class="space-y-2">
-                            <li><a href="https://www.framesdirect.com/" class="hover:underline">AMVI</a></li>
-                            <li><a href="https://www.readers.com/" class="hover:underline">HITHVI</a></li>
-                            <li><a href="https://www.readers.com/" class="hover:underline">HITSAY</a></li>
+                            <li><a href="https://www.framesdirect.com/" class="hover:underline">AMVI Eyeware</a></li>
+                            <li><a href="https://www.readers.com/" class="hover:underline">HITHVI Eyeware</a></li>
+                            <li><a href="https://www.readers.com/" class="hover:underline">HITSAY Eyeware</a></li>
 
                         </ul>
                     </div>
                 </div>
 
                 <div class="border-t border-gray-700 mt-8 pt-4 text-center">
-                    <p class="text-sm">© 2010-2025 eyebuydirect.com | Crafted by Techvision Digital</p>
+                    <p class="text-sm">© 2010-2025 amvieyewear.com | Crafted by Techvision Digital</p>
                     <ul class="flex justify-center space-x-4 mt-2 md:flex-row flex-col">
                         <li><a href="https://www.eyebuydirect.com/privacy-policy" class="hover:underline text-sm">Privacy Policy</a></li>
                         <li><a href="https://www.eyebuydirect.com/terms-and-conditions" class="hover:underline text-sm">Terms and Conditions of Use</a></li>
